4f448b187c496a25e1bcc8d31ba842579506d3ee
Ben Shurts <benjaminshurts@gmail.com>
2018-10-10 19:53:23 -0600

    particle work

---
+++ gamescripting_01/Assets/Scripts/pixelCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pixelCamera : MonoBehaviour {
+	public Camera pixelCam;
+	    public void Awake()
+    {
+        //
+		pixelCam = GetComponent<Camera>();
+        //pixelCam = (Screen.height / 32f / 2.0f); // 100f is the PixelPerUnit that you have set on your sprite. Default is 100.
+		
+    }
+
+}
