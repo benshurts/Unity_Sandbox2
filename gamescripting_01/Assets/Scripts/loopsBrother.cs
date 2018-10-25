3fd5fa692e275ead83365f967aa274d45b3f5d5c
Ben Shurts <benjaminshurts@gmail.com>
2018-10-24 18:15:09 -0600

    the loops

---
+++ gamescripting_01/Assets/Scripts/loopsBrother.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loopsBrother : MonoBehaviour {
+
+public GameObject TheLoops;
+public Sprite Sprite1;
+public Sprite Sprite2;
+public Sprite Sprite3;
+public Sprite Sprite4;
+
+//loop array
+private Sprite[] SpriteList = {};
+
+    public GameObject Spawn;
+    	private void Start() {
+
+    		for (int i = 0; i < 4; i++)
+    		{
+    			
+    		}
+    	}
+}
