9d07a9c78a3dd292672dabed9345cc7ebc2fdf4a
Ben Shurts <benjaminshurts@gmail.com>
2018-10-31 18:12:10 -0600

    raycast patrol

--- gamescripting_01/Assets/Scripts/Weapon.cs
+++ gamescripting_01/Assets/Scripts/Weapon.cs
@@ -8,7 +8,7 @@ public class Weapon : MonoBehaviour {
 
 	public float Damage = 10;
 
-	public LayerMask notToHit;
+	public LayerMask WhatToHit;
 
 	private float timeToFire = 0f;
 	private Transform firePoint;
@@ -21,15 +21,31 @@ public class Weapon : MonoBehaviour {
 		}
 	}
 	private void Update() {
+		Shoot();
 		if(fireRate == 0) {
 			if(Input.GetMouseButtonDown(0)) {
 				Shoot();
 			} else {
-				if(Input.GetMouseButton(0)) && Time.time > timeToFire {
+				if(Input.GetMouseButton(0) && Time.time > timeToFire) {
 					timeToFire = Time.time + 1/fireRate;
 					Shoot();
 				}
 			}
 		}
+	}
+
+	void Shoot() {
+		//creating a vector2 towards mouse position
+		Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		//taking firepoint of tip of gun as vector2
+		Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+		//raycast
+		RaycastHit2D hit2D = Physics2D.Raycast(firePointPosition,mousePosition-firePointPosition,100,WhatToHit);
+		//debug with gizmos to see line
+		Debug.DrawLine(firePointPosition,(mousePosition-firePointPosition)*100);
+		//show collision
+		// if(hit.collider != null) {
+		// 	Debug.DrawLine(firePointPosition, hit.point, color.red); 
+		// }
 	}
 }
