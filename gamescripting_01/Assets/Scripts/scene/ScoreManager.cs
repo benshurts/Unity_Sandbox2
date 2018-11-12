e87cb91f5b5038f9c2142b550749cf7390263038
Ben Shurts <benjaminshurts@gmail.com>
2018-11-12 16:38:23 -0700

    organize files

---
+++ gamescripting_01/Assets/Scripts/scene/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int Score;
+
+
+	Text ScoreText;
+
+	//Init
+	void Start() {
+		ScoreText = GetComponent<Text>();
+
+		Score = 0;
+	}
+
+	//Step
+	void Update() {
+		if (Score < 0) {
+			Score = 0;
+		ScoreText.text = " " + Score;
+		}
+	}
+
+	public static void AddPoints (int PointsToAdd) {
+		Score += PointsToAdd;
+	}
+
+	//public static void reset
+	
+}
