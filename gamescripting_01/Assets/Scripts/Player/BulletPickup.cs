0861497ee30591eb8ce1d4974ec2128c84b62372
Ben Shurts <benjaminshurts@gmail.com>
2018-12-08 01:29:29 -0700

    bullet pickups

--- gamescripting_01/Assets/Scripts/Player/BulletPickup.cs
+++ gamescripting_01/Assets/Scripts/Player/BulletPickup.cs
@@ -8,28 +8,14 @@ public class BulletPickup : MonoBehaviour {
 	bool CanPickup = false;
 	float TimeToPickup = 1f;
 	private void Start() {
-		ammo = GameObject.Find("GameManager").GetComponent<levelManager>().ammo;
+		// ammo = GameObject.Find("GameManager").GetComponent<levelManager>().ammo;
 
 	}
-
-	private void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.tag.Equals("Player") && TimeToPickup == 0) {
-			print("in colider");
-			CanPickup = true;
-			if(CanPickup && (Input.GetKey("e"))){
-
-				print("pickup");
-				ammo += 1;
-				Destroy(gameObject);
-			}
-
-			print("Press E to pick up");
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.tag.Equals("Player")) {
+			ammo = GameObject.Find("GameManager").GetComponent<levelManager>().ammo += 1;
+			Destroy(gameObject);
 		}
 	}
-	private void Update() {
-		TimeToPickup -= Time.deltaTime;
-		if (TimeToPickup < 0) TimeToPickup = 0;
-		// print(TimeToPickup);
 
-	}
 }
